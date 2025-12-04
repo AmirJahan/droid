d037ab8ef3d0835d05c90cb822f25c390c51e514
Amir J <amir.jahan@gmail.com>
2025-12-03 17:39:31 -0800

    classes and objects added

---
+++ ClassesandObjectsinKotlin/app/src/main/java/com/vfs/classesandobjectsinkotlin/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.vfs.classesandobjectsinkotlin
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import java.util.UUID
+
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+
+        val usr = User ("Amir", "amir@gmail.com", UUID.randomUUID())
+
+        Log.d("MainActivity", usr.email)
+    }
+}
+
+class User (var displayName: String, val email: String, val id: UUID)
+
+
+
+
+
+
+
+
+
+
+
