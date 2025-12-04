bbd0ebe3998a6e7d45d1260433b8c6ae582ea8a1
Amir J <amir.jahan@gmail.com>
2025-12-03 19:53:10 -0800

    OOP

--- OOPinKotlin/app/src/main/java/com/vfs/oopinkotlin/MainActivity.kt
+++ OOPinKotlin/app/src/main/java/com/vfs/oopinkotlin/MainActivity.kt
@@ -6,6 +6,47 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+
+// default class type is final, cannot be extended
+open class Property
+{
+    val address: String = ""
+    open fun setOpenHouse () { } // open functions can be overriden
+
+    open var ready: Boolean = false
+}
+
+class Sales () : Property ()
+{
+    override var ready: Boolean = false
+    override fun setOpenHouse()
+    {
+        super.setOpenHouse()
+    }
+}
+
+class Rentals () : Property ()
+{
+    override var ready: Boolean = true
+    override fun setOpenHouse()
+    {
+        super.setOpenHouse()
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class MainActivity : AppCompatActivity()
 {
     override fun onCreate(savedInstanceState: Bundle?)
@@ -13,10 +54,5 @@ class MainActivity : AppCompatActivity()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
